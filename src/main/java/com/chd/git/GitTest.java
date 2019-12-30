4cc5791d64d1228ae3208b4be6553fd6dcf8fe16
Haodong Chen <1284760978@qq.com>
2019-12-30 20:12:13 +0800

    Initial commit

---
+++ src/main/java/com/chd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.chd.git;
+
+/**
+ * @author Haodong Chen
+ * @version 1.0
+ * @date 2019/12/30 20:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("ccc");
+    }
+}
